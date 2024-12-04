39757b4b0796c3eb49d529ba56dbd617590fc23c
Risqi Fahreal <106820529+Xfhreall@users.noreply.github.com>
2024-12-04 22:38:07 +0700

    config: unlock on phone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Display from "@/components/ui/display";
 export default function Home() {
   return (
     <section className="relative min-h-screen w-full grid">
-      <div className="absolute self-center mx-auto w-full text-center font-bold block md:hidden">
+      {/* <div className="absolute self-center mx-auto w-full text-center font-bold block md:hidden">
         <h1>{`Gunakan laptop untuk melihat web :D`}</h1>
-      </div>
-      <div className="hidden md:block">
+      </div> */}
+      <div className="block">
         <Display />
       </div>
     </section>
